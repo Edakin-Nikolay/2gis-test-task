43e75afe1a0d16695a4df12907c4480ba3c87052
Nikolay <nikswat@mail.ru>
2021-05-17 04:18:38 +0700

    [ADD] функция загрузки 30000 книг

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,6 +5,12 @@ export const getBooks = () => {
         .then((resp: Response) => resp.json())
 }
 
+
+export const get30000Books = () => {
+    return fetch("https://raw.githubusercontent.com/lastw/test-task/master/data/30000-items.json")
+        .then((resp: Response) => resp.json())
+}
+
 export const saveToLocalStorage = (books: Array<Book>) => {
     const localStorageBooks: Array<LocalStorageBook> = books.map(book => ({id: book.id, status: book.status}));
     localStorage.setItem("books", JSON.stringify(localStorageBooks));
